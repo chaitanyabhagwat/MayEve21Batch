77fe0d1f963e1bb803f7e4fefd796c5c6dd578f6
chaitanyabhagwat <90319539+chaitanyabhagwat@users.noreply.github.com>
2021-09-08 16:40:30 +0530

    NewGitHub

---
+++ GroupJ/src/main/java/VCTC/GroupJ/App.java
@@ -0,0 +1,13 @@
+package VCTC.GroupJ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
